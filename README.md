05b38dae5b21324e125192f1c898fb613705435a
Ayoub-Younes <ayoub.jonah01@gmail.com>
2024-09-01 16:43:35 +0200

    Initial commit for telephone-number-validator

---
+++ README.md
@@ -0,0 +1,13 @@
+# Roman Numeral Converter
+
+This is aRoman Numeral Converter created as part of my JavaScript Algorithms and Data Structures Certification from FreeCodeCamp. The project demonstrates the ability to build an interactive webpage using HTML, CSS, and JavaScript.
+
+## Project Overview
+
+The Roman Numeral Converter allows users to input a number between 1 and 3999 and converts it into Roman numerals. The webpage features a visually appealing design with a background image, custom fonts, and interactive elements. The project focuses on client-side form validation and conversion logic.
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
---
+++ script.js
@@ -0,0 +1,24 @@
+const number = document.getElementById("user-input");
+const check = document.getElementById("check-btn");
+const clear = document.getElementById("clear-btn");
+const result = document.getElementById("results-div");
+const regone = /^1 \d{3}-\d{3}-\d{4}$/;
+const regtwo = /^1 \(\d{3}\) \d{3}-\d{4}$/;
+const regthree = /^1\(\d{3}\)\d{3}-\d{4}$/;
+const regfour = /^1 \d{3} \d{3} \d{4}$/;
+const regfive = /^\d{10}$/;
+const regsix = /^\d{3}-\d{3}-\d{4}$/;
+const regseven = /^\(\d{3}\)\d{3}-\d{4}$/;
+const regex = [regone,regtwo,regthree,regfour,regfive,regsix,regseven];
+const test = num => regex.some(reg => reg.test(num))
+const checkNum = () => {
+if(!number.value){
+  return alert("Please provide a phone number")
+  } else if(test(number.value)){
+  result.innerHTML = `Valid US number: ${number.value}`
+  } else {
+  result.textContent = `Invalid US number: ${number.value}`
+  }
+}
+check.addEventListener("click", checkNum);
+clear.addEventListener("click", () => result.textContent = "");
