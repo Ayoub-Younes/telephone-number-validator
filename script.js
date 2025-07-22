138e65e7d70c2658081dd1e7e7df9a478a124562
Ayoub-Younes <ayoub.jonah01@gmail.com>
2025-07-22 19:04:55 +0100

    update git

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const number = document.getElementById("user-input");
 const check = document.getElementById("check-btn");
 const clear = document.getElementById("clear-btn");
 const result = document.getElementById("results-div");
+const dialButtons = document.querySelectorAll(".dial-button")
+
 const regone = /^1 \d{3}-\d{3}-\d{4}$/;
 const regtwo = /^1 \(\d{3}\) \d{3}-\d{4}$/;
 const regthree = /^1\(\d{3}\)\d{3}-\d{4}$/;
@@ -11,6 +13,7 @@ const regsix = /^\d{3}-\d{3}-\d{4}$/;
 const regseven = /^\(\d{3}\)\d{3}-\d{4}$/;
 const regex = [regone,regtwo,regthree,regfour,regfive,regsix,regseven];
 const test = num => regex.some(reg => reg.test(num))
+
 const checkNum = () => {
 if(!number.value){
   return alert("Please provide a phone number")
@@ -20,5 +23,22 @@ if(!number.value){
   result.textContent = `Invalid US number: ${number.value}`
   }
 }
+
+dialButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    number.value += button.id;
+  });
+});
+
 check.addEventListener("click", checkNum);
-clear.addEventListener("click", () => result.textContent = "");
+clear.addEventListener("click", () => {
+  result.textContent = ""
+  number.value = ""
+});
+
+number.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    checkNum();
+  }
+  
+});
